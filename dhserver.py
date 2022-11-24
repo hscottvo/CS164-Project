682a0c53bc0ef4465fdfc8739a003fcb81b3951b
Scott <hscottvo@gmail.com>
2022-11-24 12:58:30 -0800

    Testing with explicit server ip

--- dhserver.py
+++ dhserver.py
@@ -1,7 +1,9 @@
 from socket import *
 
-DHCP_SERVER = ("", 67)
+DHCP_SERVER = ("192.168.0.1", 67)
 DHCP_CLIENT = ("255.255.255.255", 68)
+
+available_ips = set(i for i in range(1, 255))
 
 # Create a UDP socket
 s = socket(AF_INET, SOCK_DGRAM)
@@ -26,4 +28,7 @@ print("Transaction ID: " + ".".join(format(x, "x") for x in msg[4:8]))
 print("client's current ip address: " + ".".join(format(x, "x") for x in msg[12:16]))
 print("yiaddr: " + ".".join(format(x, "x") for x in msg[16:20]))
 # Send a UDP message (Broadcast)
+
+reply = b"0" * len(msg)
+
 s.sendto(b"Hello World!", DHCP_CLIENT)
