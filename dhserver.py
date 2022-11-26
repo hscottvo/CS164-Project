ace26fe443047930a89bbef37ab3bc00ba7426bf
scott <hscottvo@gmail.com>
2022-11-26 01:22:16 -0800

    Added ip distribution

--- dhserver.py
+++ dhserver.py
@@ -4,6 +4,7 @@ DHCP_SERVER = ("", 67)
 DHCP_CLIENT = ("255.255.255.255", 68)
 
 subnet = [192, 168, 0]
+server_ip = subnet + [1]
 
 available_hosts = set(i for i in range(1, 255))
 open_requests = set()
@@ -21,17 +22,23 @@ while True:
     # Recieve a UDP message
     msg, addr = s.recvfrom(1024)
     transaction_id = msg[4:8]
+    mac_address = list(msg[28:44])
+    msg_list = list(msg)
+    for i in range(len(msg)//4):
+	    print(i, list(msg[i*4:min(i*4+4, len(msg))]))
+
+    print(list(msg))
 
     reply = [0] * len(msg)
     # request
     if transaction_id in open_requests:
-        print("Got request from transaction id" + str(transaction_id) + ". Sending ack")
+        print("Got request from transaction id " + str(list(transaction_id)) + ". Sending ack")
         yiaddr = msg[16:20]
 
     # discover
     else:
         print(
-            "got discover from transaction id" + str(transaction_id) + ". Sending offer"
+            "got discover from transaction id " + str(list(transaction_id)) + ". Sending offer"
         )
         open_requests.add(transaction_id)
         host = available_hosts.pop()
@@ -40,13 +47,18 @@ while True:
 
     assert len(transaction_id) == 4
     assert len(yiaddr) == 4
-    reply = bytes(
-        reply[:2]
-        + [len(msg)]
+    reply = (
+        [2, 1]
+        + [6]
         + [reply[3]]
         + list(transaction_id)
         + reply[8:16]
         + ip_bytes
+	+ server_ip
+	+ reply[24:28]
+	+ mac_address
+	+ reply[44:]
     )
     # Send a UDP message (Broadcast)
-    s.sendto(reply, DHCP_CLIENT)
+    s.sendto(bytes(reply), DHCP_CLIENT)
+    # s.sendto(bytes(reply), ('.'.join(str(i) for i in mac_address[:4]), 68))
