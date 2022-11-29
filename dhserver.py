f7b630000dd2fa63fac38317e427fe28f4017284
Scott <hscottvo@gmail.com>
2022-11-28 22:16:02 -0800

    Fixed output

--- dhserver.py
+++ dhserver.py
@@ -67,7 +67,7 @@ while True:
     print("Sending. Currently cached ip's: ")
     for i in cached_ip:
         print(list(i))
-        print("\t", ".".join([str(j) for j in list(i)]), sep="")
+        print("\t", ".".join(i.strip("[]").split(", ")), sep="")
 
     # Send a UDP message (Broadcast)
     s.sendto(bytes(reply), DHCP_CLIENT)
